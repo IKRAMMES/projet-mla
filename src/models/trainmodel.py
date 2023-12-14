254406d8516db8ff5cdf7e38b5547aecdee84887
MohamedLokmane <mohamed_islam.lokmane@etu.sorbonne-universite.fr>
2023-12-14 17:31:35 +0100

    add comments on trainmodel.py

--- src/models/trainmodel.py
+++ src/models/trainmodel.py
@@ -7,6 +7,26 @@ import random
 from data_proc import *
 
 class Seq2SeqTrainer:
+    """
+    Utility for training sequence-to-sequence models.
+
+    Args:
+    - encoder_model (nn.Module): The encoder module.
+    - decoder_model (nn.Module): The decoder module.
+    - max_sequence_length (int): The maximum length of the output sequence.
+    - start_token (int): The token representing the start of a sequence.
+    - end_token (int): The token representing the end of a sequence.
+    - input_lang (Lang): The language object for input sentences.
+    - output_lang (Lang): The language object for target sentences.
+
+    Methods:
+    - train(input_sequence, target_sequence, encoder_optimizer, decoder_optimizer, loss_criterion, probability=0.5):
+        Trains the models for a given input-target sequence pair.
+    - custom_training_function(num_epochs, pairs, print_interval=500, plot_interval=50, learning_rate=0.01):
+        Custom training function for the models over multiple epochs.
+    - calculate_time_elapsed(start, progress):
+        Calculates the elapsed and remaining time during training.
+    """
     def __init__(self, encoder_model, decoder_model, max_sequence_length, start_token, end_token, input_lang ,output_lang , device):
         self.encoder_model = encoder_model
         #self.decoder_type = decoder_type
@@ -27,6 +47,20 @@ class Seq2SeqTrainer:
             raise ValueError("Invalid decoder type. Supported types are 'simple' and 'attention'.")'''
 
     def train(self, input_sequence, target_sequence, encoder_optimizer, decoder_optimizer, loss_criterion, probability=0.5):
+        """
+        Trains the encoder-decoder models for a given input-target sequence pair.
+
+        Args:
+        - input_sequence (torch.Tensor): Tensor representing the input sequence.
+        - target_sequence (torch.Tensor): Tensor representing the target sequence.
+        - encoder_optimizer (torch.optim.Optimizer): Optimizer for the encoder model.
+        - decoder_optimizer (torch.optim.Optimizer): Optimizer for the decoder model.
+        - loss_criterion (torch.nn.Module): Loss criterion for training.
+        - probability (float): Probability for exploration during training.
+
+        Returns:
+        - float: Normalized loss for the given sequence pair.
+        """
         # Initializations
         input_len = input_sequence.size(0)
         target_len = target_sequence.size(0)
@@ -72,6 +106,19 @@ class Seq2SeqTrainer:
         return total_loss.item() / target_len
 
     def custom_training_function(self, num_epochs, pairs, print_interval=500, plot_interval=50, learning_rate=0.01):
+        """
+        Custom training function for the encoder-decoder models.
+
+        Args:
+        - num_epochs (int): Number of training epochs.
+        - pairs (list): List of input-target sequence pairs.
+        - print_interval (int): Interval for printing training progress.
+        - plot_interval (int): Interval for plotting training losses.
+        - learning_rate (float): Learning rate for optimization.
+
+        Returns:
+        - float: Average loss over the training process.
+        """
         # Initialize loss tracking variables
         all_losses = []
         current_loss_sum = 0
@@ -111,6 +158,16 @@ class Seq2SeqTrainer:
 
     # Function to calculate elapsed and remaining time
     def calculate_time_elapsed(self, start, progress):
+        """
+        Calculates the elapsed and remaining time during training.
+
+        Args:
+        - start (float): Start time of training.
+        - progress (float): Training progress as a fraction.
+
+        Returns:
+        - str: Formatted string indicating elapsed and remaining time.
+        """
         elapsed_seconds = time.time() - start
         remaining_seconds = (elapsed_seconds / progress) * (1 - progress)
         return f'Time Elapsed: {int(elapsed_seconds)}s, Remaining: {int(remaining_seconds)}s'
