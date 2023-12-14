8482b1d0546d59d7c9cf6e0327663ca63e2ed286
MohamedLokmane <mohamed_islam.lokmane@etu.sorbonne-universite.fr>
2023-12-14 17:30:49 +0100

    Add comments for data preparation

--- src/models/data_preparation.py
+++ src/models/data_preparation.py
@@ -1,35 +1,48 @@
-
-
-# supprimer les lignes vides des deux fichiers texte
-
-'''
-# Garder que les 2000 premieres lignes
-
+# Function to remove empty lines and keep only the first n lines from two text files ( for quick trainning)
 def keep_first_n_lines_and_remove_empty(input_file, output_file, n=2000):
+    """"
+    Reads the specified number of lines (n) from the input file, removes empty lines,
+    and writes the cleaned lines to the output file.
+
+    Parameters:
+    - input_file (str): The path to the input text file.
+    - output_file (str): The path to the output text file.
+    - n (int): The number of lines to keep. Default is 2000.
+    """
+    # Open the input and output files with proper encoding
     with open(input_file, 'r', encoding='utf-8') as in_file, \
          open(output_file, 'w', encoding='utf-8') as out_file:
 
         lines_written = 0
 
-        # Lit les premières n lignes du fichier d'entrée et les écrit dans le fichier de sortie
+        # Read the first n lines from the input file and write them to the output file
         while lines_written < n:
             line = in_file.readline()
             
-            # Vérifie si la ligne est vide et la saute
+            # Check if the line is empty and skip it
             if not line.strip():
                 continue
             
             out_file.write(line)
             lines_written += 1
 
-# Utilisation de la fonction avec les noms de fichiers appropriés
+# Usage of the function with appropriate file names
+# keep_first_n_lines_and_remove_empty('news-commentary-v9.fr-en.en', 'fr2000.txt', n=2000)
+# keep_first_n_lines_and_remove_empty('news-commentary-v9.fr-en.fr', 'ang2000.txt', n=2000)
 
-keep_first_n_lines_and_remove_empty('news-commentary-v9.fr-en.en', 'fr2000.txt', n=2000)
-keep_first_n_lines_and_remove_empty('news-commentary-v9.fr-en.fr', 'ang2000.txt', n=2000)
-'''
 
-# Fonction pour combiner les deux fichiers des données
+# Function to combine data from two files
 def combine_files(file1, file2, output_file):
+    """
+    Combines the content of two text files line by line, separated by a tabulation,
+    and writes the combined lines to an output file.
+
+    Parameters:
+    - file1 (str): The path to the first input text file.
+    - file2 (str): The path to the second input text file.
+    - output_file (str): The path to the output text file.
+    """
+    # Open the input files and the output file with proper encoding
     with open(file1, 'r', encoding='utf-8') as f1, \
          open(file2, 'r', encoding='utf-8') as f2, \
          open(output_file, 'w', encoding='utf-8') as out_file:
@@ -37,17 +50,17 @@ def combine_files(file1, file2, output_file):
         lines_file1 = f1.readlines()
         lines_file2 = f2.readlines()
 
-        # Vérifie si le nombre de lignes dans les deux fichiers est le même
+        # Check if the number of lines in both files is the same
         if len(lines_file1) != len(lines_file2):
             print("Erreur: Le nombre de lignes dans les fichiers n'est pas le même.")
             return
 
-        # Combine les lignes en utilisant une tabulation entre chaque paire
+        # Combine lines using a tabulation between each pair and write to the output file
         for line1, line2 in zip(lines_file1, lines_file2):
             combined_line = f"{line1.strip()}\t{line2.strip()}\n"
             out_file.write(combined_line)
 
-# Utilisation de la fonction avec les noms de fichiers appropriés
-combine_files('eng2000.txt', 'fra2000.txt', 'fra-eng.txt')
+# Usage of the function with appropriate file names
+# combine_files('eng2000.txt', 'fra2000.txt', 'fra-eng.txt')
 
 
